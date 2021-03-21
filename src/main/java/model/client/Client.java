5d587e5a91a0635a2fe2829e2251b2e06f4280da
Chiara Tozzi <chiara.tozzi2@studio.unibo.it>
2021-03-21 16:12:27 +0100

    added methods to the interface

--- src/main/java/model/client/Client.java
+++ src/main/java/model/client/Client.java
@@ -1,5 +1,33 @@
 package model.client;
 
 public interface Client {
+    
+    /**
+     * Insert the costumer in the file.
+     * @param name
+     * @param surname
+     * @param id
+     */
+    void insertClient(String name, String surname, String id ); 
+    
+    /**
+     * Get an existing customer from the file.
+     * @param id
+     * @return The Client id
+     */
+    String getClient(String id);
+    
+    /**
+     * Search if a client is present.
+     * @param id
+     * @return true if present if not false
+     */
+    Boolean searchClient(String id);
+    
+    /**
+     * Search in which room the customer is staying
+     * @param id
+     */
+    String getRoom(String id);
 
 }
