6d2b8be2d8058b74b69f039a464574711a385b47
Simo <simone.albonetti@studio.unibo.it>
2021-04-11 01:30:45 +0200

    Update AddReservationView and ReservationHomeView

--- src/main/java/view/Reservation/AddReservationView.java
+++ src/main/java/view/Reservation/AddReservationView.java
@@ -1,89 +1,131 @@
 package view.Reservation;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTextField;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JButton;
 import com.toedter.calendar.JDateChooser;
-import net.miginfocom.swing.MigLayout;
+import java.awt.Font;
+import java.awt.Color;
+
 
 public class AddReservationView extends JFrame {
     private final JPanel infoPanel = new JPanel();
-    private final JPanel labelPanel = new JPanel();
     private final JPanel buttonPanel = new JPanel();
-    private JTextField textField;
-    private JTextField textField_2;
-    private JTextField textField_3;
+    private final JTextField roomTextField = new JTextField();
+    private final JTextField clientTextField = new JTextField();
+    private final JLabel infoLabel = new JLabel("Inserire i dati per confermare la prenotazione");
+    private final JButton saveButton = new JButton("Salva");
+    private final JButton cancelButton = new JButton("Annulla");
+    private final JPanel dataPanel = new JPanel();
+    private final JLabel clientLabel = new JLabel("Cliente");
+    private final JLabel checkInLabel = new JLabel("Data check-in");
+    private final JLabel checkOutLabel = new JLabel("Data check-out");
+    private final JLabel roomLabel = new JLabel("Stanza");
+    private final JDateChooser checkInDateChooser = new JDateChooser();
+    private final JDateChooser checkOutDateChooser = new JDateChooser();
+    private final JButton clientButton = new JButton("Scegli");
+    private final FlowLayout infoPanelLayout = new FlowLayout();
+    private final BorderLayout frameLayout = new BorderLayout();
+    private final FlowLayout buttonPanelLayout = new FlowLayout();
+    private final GroupLayout dataPanelLayout = new GroupLayout(dataPanel);
+    private final JButton roomButton = new JButton("Scegli");
+
 
     /**
      * Create the frame.
      */
     public AddReservationView() {
-        setTitle("Aggiungi prenotazione");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 542, 430);
-        FlowLayout flowLayout = (FlowLayout) infoPanel.getLayout();
-        
-        this.getContentPane().add(infoPanel, BorderLayout.NORTH);
-        
-        
-        
-        JLabel lblNewLabel_4 = new JLabel("Inserire i dati per confermare la prenotazione");
-        infoPanel.add(lblNewLabel_4);
-        
-        this.getContentPane().add(labelPanel, BorderLayout.CENTER);
-        labelPanel.setLayout(new MigLayout("", "[68px][328px]", "[19px][19px][19px][19px]"));
-        
-        JLabel lblNewLabel = new JLabel("Cliente");
-        labelPanel.add(lblNewLabel, "cell 0 0,alignx center,aligny center");
-        
-        textField = new JTextField();
-        labelPanel.add(textField, "cell 1 0,growx,aligny center");
-        textField.setColumns(10);
-        
-        JLabel lblNewLabel_1 = new JLabel("Data check-in");
-        labelPanel.add(lblNewLabel_1, "cell 0 1,alignx center,aligny center");
-        
-        JDateChooser dateChooser = new JDateChooser();
-        BorderLayout borderLayout = (BorderLayout) dateChooser.getLayout();
-        labelPanel.add(dateChooser, "cell 1 1,growx,aligny center");
-        
-        JLabel lblNewLabel_2 = new JLabel("Data check-out");
-        labelPanel.add(lblNewLabel_2, "cell 0 2,alignx center,aligny center");
-        
-        textField_2 = new JTextField();
-        labelPanel.add(textField_2, "cell 1 2,growx,aligny center");
-        textField_2.setColumns(10);
-        
-        JLabel lblNewLabel_3 = new JLabel("Stanza");
-        labelPanel.add(lblNewLabel_3, "cell 0 3,alignx center,aligny center");
-        
-        textField_3 = new JTextField();
-        labelPanel.add(textField_3, "cell 1 3,growx,aligny center");
-        textField_3.setColumns(10);
-        FlowLayout flowLayout_1 = (FlowLayout) buttonPanel.getLayout();
-        flowLayout_1.setAlignment(FlowLayout.RIGHT);
-        
-        this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-        
-        JButton btnNewButton = new JButton("Annulla");
-        buttonPanel.add(btnNewButton);
-        
-        JButton btnNewButton_1 = new JButton("Salva");
-        buttonPanel.add(btnNewButton_1);
-        
-    }
+        //Initialize the frame
+        this.getContentPane().setLayout(frameLayout);
+        this.frameLayout.setVgap(23);
+        this.frameLayout.setHgap(66);
+        this.setTitle("Aggiungi prenotazione");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(100, 100, 542, 430);
 
+        //Initialize the north panel
+        this.getContentPane().add(infoPanel, BorderLayout.NORTH);
+        this.infoPanel.setLayout(infoPanelLayout);
+        this.infoPanelLayout.setVgap(20);
+        this.infoLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        this.infoPanel.add(infoLabel);
+
+        //Initialize the south panel
+        this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        this.buttonPanel.setLayout(buttonPanelLayout);
+        this.buttonPanelLayout.setAlignment(FlowLayout.RIGHT);
+        this.buttonPanel.add(cancelButton);
+        this.buttonPanel.add(saveButton);
+
+        //Initialize center panel
+        this.getContentPane().add(dataPanel, BorderLayout.CENTER);
+        this.clientLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        this.checkInLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        this.checkOutLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        this.roomLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        this.roomTextField.setColumns(10);
+        this.clientTextField.setColumns(10);
+
+       
+        this.dataPanelLayout.setHorizontalGroup(
+            dataPanelLayout.createParallelGroup(Alignment.LEADING)
+                .addGroup(dataPanelLayout.createSequentialGroup()
+                    .addGap(36)
+                    .addGroup(dataPanelLayout.createParallelGroup(Alignment.LEADING, false)
+                        .addComponent(clientLabel, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(checkInLabel, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(checkOutLabel)
+                        .addComponent(roomLabel, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+                    .addGap(36)
+                    .addGroup(dataPanelLayout.createParallelGroup(Alignment.LEADING)
+                        .addGroup(dataPanelLayout.createSequentialGroup()
+                            .addGroup(dataPanelLayout.createParallelGroup(Alignment.LEADING, false)
+                                .addComponent(roomTextField)
+                                .addComponent(checkOutDateChooser, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(checkInDateChooser, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGap(18)
+                            .addComponent(roomButton, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE))
+                        .addGroup(dataPanelLayout.createSequentialGroup()
+                            .addComponent(clientTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(18)
+                            .addComponent(clientButton)))
+                    .addContainerGap(147, Short.MAX_VALUE))
+        );
+        dataPanelLayout.setVerticalGroup(
+            dataPanelLayout.createParallelGroup(Alignment.LEADING)
+                .addGroup(dataPanelLayout.createSequentialGroup()
+                    .addGap(34)
+                    .addGroup(dataPanelLayout.createParallelGroup(Alignment.LEADING)
+                        .addComponent(clientLabel)
+                        .addGroup(dataPanelLayout.createParallelGroup(Alignment.BASELINE)
+                            .addComponent(clientTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(clientButton)))
+                    .addGap(18)
+                    .addGroup(dataPanelLayout.createParallelGroup(Alignment.LEADING)
+                        .addGroup(dataPanelLayout.createSequentialGroup()
+                            .addComponent(checkInDateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(18)
+                            .addComponent(checkOutDateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(18)
+                            .addGroup(dataPanelLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(roomTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(roomButton)))
+                        .addGroup(dataPanelLayout.createSequentialGroup()
+                            .addComponent(checkInLabel)
+                            .addGap(18)
+                            .addComponent(checkOutLabel)
+                            .addGap(18)
+                            .addComponent(roomLabel)))
+                    .addContainerGap(115, Short.MAX_VALUE))
+        );
+        dataPanel.setLayout(dataPanelLayout);
+    }
 }
--- src/main/java/view/Reservation/ReservationsHomeView.java
+++ src/main/java/view/Reservation/ReservationsHomeView.java
@@ -9,17 +9,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 public class ReservationsHomeView {
 
     private JFrame reservationsFrame = new JFrame();
-    private JPanel buttonsPanel = new JPanel();
-    private JPanel textAreaPanel = new JPanel();
+    private JPanel buttonsPanel;
+    private JPanel buttonsPanel_1 = new JPanel();
     private JButton viewReservationButton = new JButton("Visualizza prenotazioni");
     private JButton addReservationButton = new JButton("Aggiungi prenotazione");
     private JButton modifyReservationButton = new JButton("Modifica prenotazione");
     private JButton deleteReservationButton = new JButton("Elimina prenotazione");
-    private final JDateChooser dateChooser = new JDateChooser();
 
 
      /**
@@ -28,35 +32,41 @@ public class ReservationsHomeView {
     public ReservationsHomeView() {
 
         this.reservationsFrame.getContentPane().setLayout(new BorderLayout());
-        this.reservationsFrame.getContentPane().add(buttonsPanel, BorderLayout.NORTH);
-        this.reservationsFrame.getContentPane().add(textAreaPanel, BorderLayout.CENTER);
+        this.reservationsFrame.getContentPane().add(buttonsPanel_1, BorderLayout.CENTER);
+        GroupLayout gl_buttonsPanel_1 = new GroupLayout(buttonsPanel_1);
+        gl_buttonsPanel_1.setHorizontalGroup(
+            gl_buttonsPanel_1.createParallelGroup(Alignment.LEADING)
+                .addGroup(Alignment.TRAILING, gl_buttonsPanel_1.createSequentialGroup()
+                    .addGap(116)
+                    .addGroup(gl_buttonsPanel_1.createParallelGroup(Alignment.TRAILING)
+                        .addComponent(deleteReservationButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
+                        .addComponent(modifyReservationButton, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
+                        .addComponent(viewReservationButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
+                        .addComponent(addReservationButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE))
+                    .addGap(139))
+        );
+        gl_buttonsPanel_1.setVerticalGroup(
+            gl_buttonsPanel_1.createParallelGroup(Alignment.LEADING)
+                .addGroup(Alignment.TRAILING, gl_buttonsPanel_1.createSequentialGroup()
+                    .addContainerGap(76, Short.MAX_VALUE)
+                    .addComponent(viewReservationButton)
+                    .addPreferredGap(ComponentPlacement.UNRELATED)
+                    .addComponent(addReservationButton)
+                    .addGap(10)
+                    .addComponent(modifyReservationButton)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(deleteReservationButton)
+                    .addGap(48))
+        );
+        buttonsPanel_1.setLayout(gl_buttonsPanel_1);
 
-
-        this.textAreaPanel.add(dateChooser);
-
-
-        this.buttonsPanel.add(viewReservationButton);
-        this.buttonsPanel.add(addReservationButton);
-        this.buttonsPanel.add(modifyReservationButton);
-        this.buttonsPanel.add(deleteReservationButton);
-
-
-        this.viewReservationButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-            }
-
-        });
         this.reservationsFrame.setTitle("Prenotazioni");
 
-
         this.reservationsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.reservationsFrame.setBounds(100, 100, 606, 338);
+        this.reservationsFrame.setBounds(100, 100, 406, 271);
         buttonsPanel = new JPanel();
         buttonsPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         buttonsPanel.setLayout(new BorderLayout(0, 0));
 
     }
-
 }
