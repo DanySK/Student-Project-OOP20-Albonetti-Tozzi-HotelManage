befea8a63439cff8e13bf195aad1164d69f1b185
Chiara Tozzi <chiara.tozzi2@studio.unibo.it>
2021-04-13 20:25:37 +0200

    updated for testing

--- src/application/FileManager.java
+++ src/application/FileManager.java
@@ -9,7 +9,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
-
+import model.client.*;
+import controller.Client.*;
 public class FileManager {
 
 public static void main(String[] args) throws FileNotFoundException, IOException{
@@ -17,20 +18,31 @@ public static void main(String[] args) throws FileNotFoundException, IOException
         String s;
         boolean b;
         List<String> list;
-        System.out.println("istanzio");
+        //System.out.println("istanzio");
 
-        final URL clientsUrl = ClassLoader.getSystemResource("Clients.txt");
+        /*final URL clientsUrl = ClassLoader.getSystemResource("Clients.txt");
         System.out.println(clientsUrl);
 
         MyFile myfile = new MyFileImpl(clientsUrl);
 
-        b = myfile.fileWriter("mario rossi asjhaf41125 12/05/1999");
+        b = myfile.fileWriter("Giovanni Giorgio GIORGIOV33E66W456T");
         System.out.println("Risultato Scrittura: " + b);
 
         list = myfile.fileReader();
         System.out.println("Risultato Lettura: " + list);
 
-        s = myfile.fileSearch("mario");
-        System.out.println("Risultato ricerca: " + s);
+        s = myfile.fileSearch("Chiara");
+        System.out.println("Risultato ricerca: " + s);*/
+        
+        ControllerClient client = new ControllerClientImpl();
+        
+        System.out.println("Get all: " + client.getAllClient());
+        System.out.println("Get client Luigi: " + client.getClient("ROSLUI99B72T898P"));
+        System.out.println("Search Giorgio: " + client.searchClient("GIORGIOV33E66W456T"));
+        System.out.println("Search Giorgio: " + client.searchClient("tzzcHR00A50R458L"));
+        System.out.println("Search Giorgio: " + client.searchClient("TZZCHR00A50R458L"));
+        
+        client.insertClient("Mario", "Neri", "MARNER65G80I548K");
+        System.out.println("Get all: " + client.getAllClient());
     } 
 }
