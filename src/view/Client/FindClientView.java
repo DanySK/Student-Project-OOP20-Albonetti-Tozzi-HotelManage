3c611242cf2f2bcac16eb22c7926fc69525cf8b0
Chiara Tozzi <chiara.tozzi2@studio.unibo.it>
2021-04-20 16:30:24 +0200

    Added the possibility to see the data of the searched customer

--- src/view/Client/FindClientView.java
+++ src/view/Client/FindClientView.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 
 import controller.Client.ControllerClient;
 import controller.Client.ControllerClientImpl;
+import model.client.Client;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -39,7 +40,10 @@ public class FindClientView extends JFrame {
     private final JLabel label = new JLabel("Inserisci il codice fiscale del cliente desiderato");
     private final JLabel lblNonTrovato = new JLabel("Non trovato");
     private final JButton buttonSearch = new JButton("Ricerca");
-    
+    private final JLabel lblCliente = new JLabel("Cliente:");
+    private final JLabel labelName = new JLabel("Name");
+    private final JLabel labelSurname = new JLabel("Surname");
+    private final JLabel labelId = new JLabel("CF");
     private String id;
     private final JButton btnElimina = new JButton("Elimina");
 
@@ -58,6 +62,10 @@ public class FindClientView extends JFrame {
         
         label.setFont(new Font("Tahoma", Font.BOLD, 16));
         lblNonTrovato.setFont(new Font("Tahoma", Font.BOLD, 12));
+        lblCliente.setFont(new Font("Tahoma", Font.BOLD, 12));
+        labelName.setFont(new Font("Tahoma", Font.BOLD, 12));
+        labelSurname.setFont(new Font("Tahoma", Font.BOLD, 12));
+        labelId.setFont(new Font("Tahoma", Font.BOLD, 12));
         textfield = new JTextField();
         textfield.setHorizontalAlignment(SwingConstants.CENTER);
         textfield.setToolTipText("Codice Fiscale");
@@ -68,8 +76,12 @@ public class FindClientView extends JFrame {
             @Override
             public void mousePressed(final MouseEvent e) {
                 id = textfield.getText();
-                if (client.searchClient(id)) {
+                if (client.searchClient(id) && !id.isEmpty()) {
                     lblNonTrovato.setText("Trovato");
+                    Client c = client.getClient(id);
+                    labelName.setText("" + c.getName());
+                    labelSurname.setText("" + c.getSurname());
+                    labelId.setText("" + c.getId());
                 }
                 else{
                     lblNonTrovato.setText("Questo codice fiscale non è presente");
@@ -92,23 +104,34 @@ public class FindClientView extends JFrame {
             }
         });
 
+
         GroupLayout glcontentPane = new GroupLayout(contentPane);
         glcontentPane.setHorizontalGroup(
             glcontentPane.createParallelGroup(Alignment.LEADING)
                 .addGroup(glcontentPane.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(glcontentPane.createParallelGroup(Alignment.LEADING)
-                        .addComponent(label, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
+                        .addComponent(label, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 924, Short.MAX_VALUE)
                         .addGroup(Alignment.TRAILING, glcontentPane.createSequentialGroup()
                             .addComponent(lblNonTrovato, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                            .addPreferredGap(ComponentPlacement.RELATED, 561, Short.MAX_VALUE)
                             .addGroup(glcontentPane.createParallelGroup(Alignment.LEADING, false)
                                 .addComponent(btnElimina, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(buttonSearch, GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
                             .addGap(10))
-                        .addGroup(glcontentPane.createSequentialGroup()
+                        .addGroup(Alignment.TRAILING, glcontentPane.createSequentialGroup()
                             .addComponent(textfield, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE)
-                            .addContainerGap(233, Short.MAX_VALUE))))
+                            .addContainerGap(743, Short.MAX_VALUE))
+                        .addGroup(glcontentPane.createSequentialGroup()
+                            .addComponent(lblCliente, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
+                            .addContainerGap(671, Short.MAX_VALUE))))
+                .addGroup(glcontentPane.createSequentialGroup()
+                    .addGap(51)
+                    .addGroup(glcontentPane.createParallelGroup(Alignment.LEADING)
+                        .addComponent(labelId, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(labelSurname, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(labelName, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE))
+                    .addContainerGap(630, Short.MAX_VALUE))
         );
         glcontentPane.setVerticalGroup(
             glcontentPane.createParallelGroup(Alignment.LEADING)
@@ -117,7 +140,15 @@ public class FindClientView extends JFrame {
                     .addComponent(label)
                     .addPreferredGap(ComponentPlacement.UNRELATED)
                     .addComponent(textfield, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(ComponentPlacement.RELATED, 121, Short.MAX_VALUE)
+                    .addPreferredGap(ComponentPlacement.RELATED, 254, Short.MAX_VALUE)
+                    .addComponent(lblCliente, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(labelName, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(labelSurname, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(labelId, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+                    .addGap(9)
                     .addComponent(btnElimina)
                     .addPreferredGap(ComponentPlacement.UNRELATED)
                     .addGroup(glcontentPane.createParallelGroup(Alignment.BASELINE)
